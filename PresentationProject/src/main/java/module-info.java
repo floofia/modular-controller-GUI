0594867d0de023d6a6739190bb9897329ff3d91d
Hannah Carroll <hannah.carroll@mines.sdsmt.edu>
2023-02-17 13:34:53 -0700

    removed old PresentationProject directory

--- PresentationProject/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.presentationproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.fazecast.jSerialComm;
-
-
-    opens presentationproject to javafx.fxml;
-    exports presentationproject;
-}
